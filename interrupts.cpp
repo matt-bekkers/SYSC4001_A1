a41f623c861b488c6d5c8ae1c2af724043bafc1a
Tomas Alvarez <tomasalvarez@mac.home>
2025-10-02 21:55:57 -0400

    fixed some of the extra code i accidently duplicated

--- interrupts.cpp
+++ interrupts.cpp
@@ -13,6 +13,12 @@
 
 
 
+static constexpr int KERNEL_SWITCH_MS = 1;
+static constexpr int CTX_SAVE_MS = 10;
+static constexpr int VECTOR_CALC_MS = 1;
+static constexpr int GET_ISR_MS = 1;
+static constexpr int ISR_BODY_MS = 40;
+static constexpr int IRET_MS = 1;
 
 int main(int argc, char** argv) {
 
@@ -28,12 +34,7 @@ int main(int argc, char** argv) {
     /******************ADD YOUR VARIABLES HERE*************************/
     
 
-    static constexpr int KERNEL_SWITCH_MS = 1;
-    static constexpr int CTX_SAVE_MS = 10;
-    static constexpr int VECTOR_CALC_MS = 1;
-    static constexpr int GET_ISR_MS = 1;
-    static constexpr int ISR_BODY_MS = 40;
-    static constexpr int IRET_MS = 1;
+    
 
     int time = 0;
     /******************************************************************/
@@ -94,42 +95,7 @@ int main(int argc, char** argv) {
                         + std::to_string(dev) + " (avg delay " + std::to_string(d) + " ms)\n";
                 time += 1;
             }
-}
- 
-        
-        
-        else if (activity == "SYSCALL" || activity == "END_IO"){
-            int dev = value;
-            std::string isr = (dev >= 0 && dev < (int)vectors.size()) ? vectors[dev] : "0x0000";
-            std::string label = (activity == "SYSCALL" ? "syscall dev" : "end_io dev") + std::to_string(dev);
-
-            //switch to kernel
-            execution += std::to_string(time) + ", " + std::to_string(KERNEL_SWITCH_MS) + ", switch to kernel mode (" + label + ")\n";
-            time += KERNEL_SWITCH_MS;
             
-            //save context
-            execution += std::to_string(time) + ", " + std::to_string(CTX_SAVE_MS) + ", context saved\n";
-            time += CTX_SAVE_MS;
-
-            // find vector entry
-            execution += std::to_string(time) + ", " + std::to_string(VECTOR_CALC_MS) + ", find vector " + std::to_string(dev) + "\n";
-            time += VECTOR_CALC_MS;
-
-            //obtain ISR address
-            execution += std::to_string(time) + ", " + std::to_string(ISR_BODY_MS) + ", execute ISR body (device " + std::to_string(dev) + ")\n";
-            time += ISR_BODY_MS;
-            // execute ISR body (kept constant to stay tiny/simple)
-            execution += std::to_string(time) + ", " + std::to_string((dev >=0 && dev < (int)delays.size()) ? delays[dev] : ISR_BODY_MS);
-
-            // IRET
-            execution += std::to_string(time) + ", " + std::to_string(IRET_MS) + ", IRET\n";
-            time += IRET_MS;
-
-            if (activity == "SYSCALL") {
-                int d = (dev >= 0 && dev < (int)delays.size()) ? delays[dev] : 0;
-                execution += std::to_string(time) + ", 1, issue I/O to device " + std::to_string(dev) + " (avg delay " + std::to_string(d) + " ms)\n";
-                time += 1;
-            }
          } else if (!activity.empty()) {
             // Unknown line: log and continue (duration 0 to keep timeline intact)
             execution += std::to_string(time) + ", 0, unknown activity: " + activity + "\n";
